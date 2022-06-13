36d83ca6f4330e2fb4e9d51bf4b65b42315bf461
GUSTAVO <90003154+gusta-fontoura@users.noreply.github.com>
2022-06-13 11:01:57 -0300

    Add files via upload
    upload js files

---
+++ Treinamento JavaScript/Anotações.js
@@ -0,0 +1,15 @@
+var y = 0; // variáveis são manipuladas
+
+const PRECO = 2; // Constantes não são manipuladas
+
+function soma(a,b) {  // declarando função
+    return a + b  // não precisa necessáriamente do return. Ele é utilizando para que você consiga executar a função em outra função.
+}
+
+soma (3,5) // chamando a função você pode chamar funções sem parâmetros.
+
+/* aprendendo a utilizar o console.
+podemos utilizar o console em uma página da web ou em um terminal... 
+Para rodar no Terminal é precisa usar o Node.JS*/
+
+console.log("Hello World");
